e4d0036ef38790f1dbdff7ccdfb2be5609aba5b0
itsMugen <77022426+itsMugen@users.noreply.github.com>
2024-02-06 19:31:21 +0100

    refactoring

--- wipro/test/test_engine.py
+++ wipro/test/test_engine.py
@@ -1,7 +1,7 @@
 import time
 import random
 
-
+# https://stackoverflow.com/questions/553303/generate-a-random-date-between-two-other-dates?page=1&tab=scoredesc#tab-top
 def str_time_prop(start, end, time_format, prop):
 	"""Get a time at a proportion of a range of two formatted times.
 	start and end should be strings specifying times formatted in the
@@ -17,19 +17,15 @@ def str_time_prop(start, end, time_format, prop):
 
 	return time.strftime(time_format, time.localtime(ptime))
 
+#create data to test the engine
 def generator():
 	with open('BIG_input.txt', 'w') as file:
 		for i in range(0, 99999999999):
-			#writwe
 			instrument = random.randrange(1, 10000)
-
 			date = str_time_prop("01-Jan-1995", "19-Dec-2014", "%d-%b-%Y", random.random())
-
 			value = round(random.uniform(0.10, 20.00), random.randrange(3, 5))
 
 			file.write(f"INSTRUMENT{instrument},{date},{value}\n")
-		
-
 
 
 if __name__ == "__main__":
